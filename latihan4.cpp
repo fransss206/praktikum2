7baa07bed090ce4a33aa1311f5916b60a0b00e85
fransss206 <fransfrans206@gmail.com>
2018-11-02 20:39:18 +0700

    udh diubah

---
+++ latihan4.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int a,n,bil,max=0;
+cout<<"masukan jumlah bilangan = ";cin>>n;
+for (a=1; a<=n; ++a)
+{
+ cout<<"Bilangan ke " << a << "  =";cin>>bil;
+ if(bil>max)
+ {
+ max=bil;
+ }
+}
+cout<<endl;
+cout<<"Bilangan Terbesar ="<<max;
+
+}
+
+
+
